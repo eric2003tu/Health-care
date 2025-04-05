75e4756493cd3b36deab26e3e27b024657861b19
eric2003tu <mukarugambwaelevanie@gmail.com>
2025-04-05 15:39:33 +0200

    github pages removed

--- src/Components/login.jsx
+++ src/Components/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect} from 'react'
 import { FaStar } from "react-icons/fa";
-import profile from '../assets/Bg.jpg';
+import profile from '../assets/BG.jpg';
 import { Eye, EyeOff,Mail, User } from "lucide-react";
 import { BrowserRouter as Navigate, Link, useNavigate } from 'react-router-dom';
 import { color } from 'framer-motion';
